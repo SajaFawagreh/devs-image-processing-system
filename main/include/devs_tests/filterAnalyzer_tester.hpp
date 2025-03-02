47ce53c5b073f5aa4d604e2be1e6f6567dc550c3
saja fawagreh <saja.fawagreh@gmail.com>
2025-03-02 15:11:10 -0500

    Updated file paths

--- main/include/devs_tests/filterAnalyzer_tester.hpp
+++ main/include/devs_tests/filterAnalyzer_tester.hpp
@@ -11,7 +11,13 @@ struct filterAnalyzer_tester: public Coupled {
 
   filterAnalyzer_tester(const std::string& id) : Coupled(id) {
     auto filterAnalyzer_model = addComponent<filterAnalyzer>("filterAnalyzer model");
-    auto filterAnalyzer_file = addComponent<lib::IEStream<std::string>>("filterAnalyzer file", "/home/sajaf/devs-image-processing-system/test_files/filterAnalyzer/test1/filterAnalyzer_test.txt");
+
+    std::string test = "test1"
+    std::string filter_input_test = "/absolute/path/to/devs-image-processing-system/test_files/filterAnalyzer/" + test + "/filterAnalyzer_test.txt";
+
+    auto filterAnalyzer_file = addComponent<lib::IEStream<std::string>>(
+      "filterAnalyzer file", filter_input_test
+    );
     
     //Internal Couplings
     addCoupling(filterAnalyzer_file->out, filterAnalyzer_model->in); // Connect test input to FilterAnalyzer
